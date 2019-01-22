bdc2dac9da399e11d13bf5a5b713e350f73ab802
LuisAlbertoZepedaHernandez <46934788+LuisAlbertoZepedaHernandez@users.noreply.github.com>
2019-01-22 11:04:24 -0600

    Luis Alberto Zepeda Hernadez, Grupo 03

---
+++ Velocidad promedio.py
@@ -0,0 +1,8 @@
+#Autor: Luis Alberto Zepeda Hernádez.
+#Calcula velocidad promedio, dado el tiempo y la distancia.
+
+distancia = int(input("Introducir distancia en kilometros enteros: "))
+tiempo = int(input("Introducir tiempo en horas enteras: "))
+
+velocidad = distancia / tiempo
+print("velocidad promedio:", velocidad, "km/hr")
